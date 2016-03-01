2651e7078081be68953d3767d8b7afff140e0efe
casimiryang <casimiryang@163.com>
2016-03-01 17:49:29 +0800

    3-1

--- crawler/crawlerIP/dbUtil.py
+++ crawler/crawlerIP/dbUtil.py
@@ -9,7 +9,7 @@ from py2neo.cypher.error.schema import ConstraintViolation
 
 watch("httpstream")  #The watch function comes with the bundled httpstream library and simply dumps log entries to standard output.
 
-graph = Graph("http://neo4j:080370053@localhost:7474/db/data/")
+graph = Graph("http://neo4j:123456@localhost:7474/db/data/")
 #graph.schema.create_uniqueness_constraint("IP", "ip")
 
 
