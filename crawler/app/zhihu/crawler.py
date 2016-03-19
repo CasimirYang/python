895f25356bb7c6d2643d02e74506af1bc1a3ef31
yjh_server <casimiryang@163.com>
2016-03-19 11:51:42 +0800

    v 3.9 request final

--- crawler/app/zhihu/crawler.py
+++ crawler/app/zhihu/crawler.py
@@ -2,38 +2,52 @@
 # -*- coding: utf-8 -*-
 import logging
 import logging.config
+import os
+import pickle
 import traceback
 
+import aiohttp
+import asyncio
 import yaml
 
 from app.zhihu import User, sent_email
 from app.zhihu.crawlerHandle import init_session, handle_follow
 from app.zhihu.dbUtil import get_user, get_total_user, get_total_relationshiop
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
 def begin_to_crawler():
-    session = init_session()
+    global client
+    file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'rb')
+    cookies = pickle.load(file)
+    file.close()
+    client = aiohttp.ClientSession()
+    client._cookies = cookies
+   # session = init_session()
     user_list = get_user()
     if len(user_list) == 0:
         default_user = User('gong-zi-winnie', '心经', 1) #todo change to fetch from config
-        handle_follow(session, default_user)
+        handle_follow(client, default_user)
         user_list = get_user()
     i = 0
     while 1:
         i += 1
-        #loop it
-        for user in user_list:
-            user.status = 1
-            handle_follow(session, user)
         logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  {0} cycle. ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^".format(i))
+        #loop it
+        #for user in user_list:
+            #user.status = 1
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait([handle_follow(user) for user in user_list]))
+        loop.close()
+           # handle_follow(client, user)
+
         if len(user_list) == 0:
             logger.info("crawler done.")
-            total_user = get_total_user()
-            total_rela = get_total_relationshiop()
-            sent_email("Crawler Info", "crawler done.<br /> "
-                                       "Summary: total user:{0} total relationship:{1}".format(total_user, total_rela))
+            # total_user = get_total_user()
+            # total_rela = get_total_relationshiop()
+            # sent_email("Crawler Info", "crawler done.<br /> "
+            #                            "Summary: total user:{0} total relationship:{1}".format(total_user, total_rela))
             return
 
         user_list = get_user()
@@ -44,7 +58,7 @@ def zhihu_crawler():
         begin_to_crawler()
     except Exception:
         logger.exception("crawler zhihu cause exception.")
-        sent_email("Crawler Exception. detmail:<br />", traceback.format_exc())
+        #sent_email("Crawler Exception. detmail:<br />", traceback.format_exc())
 
 
 
--- crawler/app/zhihu/crawlerHandle.py
+++ crawler/app/zhihu/crawlerHandle.py
@@ -20,17 +20,15 @@ from app.crawlerIP.dbUtil import get_proxy_ip_host
 from app.zhihu import User, UserDetail, log_time, sent_email
 from app.zhihu.dbUtil import save_user, save_follow, update_user_status
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
-headers = {"Host": "www.zhihu.com",
-           "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36",
-           "Referer": "http://www.zhihu.com/",
-           "Origin": "http://www.zhihu.com",
-           "accept-encoding": "gzip, deflate"
-           }
 
 data = {"remember_me": "true", "email": "tianxiaofu3@sina.com", "password": "txf1456123"}
 proxy = {"http": "http://58.83.174.114:80"}
+
+file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'rb')
+cookies = pickle.load(file)
+file.close()
 
 func = get_proxy_ip_host()
 
@@ -41,75 +39,81 @@ def get_proxy():
     return {"http": "http://{0}:{1}".format(ip, host)}
 
 
-async def loginByAiohttp():
-    global client
-    global cookie
-    data = {"remember_me": "true", "email": "tianxiaofu2@sina.com", "password": "txf1456123"}
-    url = "http://www.zhihu.com/login/email"
-    conn = aiohttp.ProxyConnector(proxy="http://119.39.191.52:80")
-    client = aiohttp.ClientSession()
-    resp = await client.post(url, headers=headers, data=data)
-    cookie = client.cookies
-    file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'wb')
-    pickle.dump(cookie, file)
-    file.close()
-    print("cookie:{0}".format(cookie))
-    print(await resp.json())
-
-    url2 = "http://www.zhihu.com/people/li-fang-quan-1/followers"
-    resp = await client.get(url2)
-    print(await resp.text())
-
-async def test(client):
-    print("test1")
-    homePageUrl = "https://www.zhihu.com/people/li-fang-quan-1/followers"
-    resp = await client.get(homePageUrl)
-    print("test2")
-    print(await resp.text())
-
-async def test2(client):
-    print("test1")
-    homePageUrl = "https://www.zhihu.com/people/li-fang-quan-1/followers"
-    resp = await client.get(homePageUrl)
-    print("test2")
-    print(await resp.text())
+def get_aio_proxy():
+    ip, host = func.__next__()
+    return aiohttp.ProxyConnector("http://{0}:{1}".format(ip, host))
 
 
-if __name__=='__main__':
-    file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'rb')
-    cookies = pickle.load(file)
-    file.close()
-    conn = aiohttp.ProxyConnector("http://222.176.112.10:80")
-    client = aiohttp.ClientSession(connector=conn, headers=headers)
-    client._cookies = cookies
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait([test(client), test2(client)]))
+# async def loginByAiohttp():
+#     global client
+#     global cookie
+#     data = {"remember_me": "true", "email": "tianxiaofu2@sina.com", "password": "txf1456123"}
+#     url = "http://www.zhihu.com/login/email"
+#     conn = aiohttp.ProxyConnector(proxy="http://119.39.191.52:80")
+#     client = aiohttp.ClientSession()
+#     resp = await client.post(url, headers=headers, data=data)
+#     cookie = client.cookies
+#     file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'wb')
+#     pickle.dump(cookie, file)
+#     file.close()
+#     print("cookie:{0}".format(cookie))
+#     print(await resp.json())
+#
+#     url2 = "http://www.zhihu.com/people/li-fang-quan-1/followers"
+#     resp = await client.get(url2)
+#     print(await resp.text())
+#
+# async def test(client):
+#     print("test1")
+#     homePageUrl = "https://www.zhihu.com/people/li-fang-quan-1/followers"
+#     resp = await client.get(homePageUrl)
+#     print("test2")
+#     print(await resp.text())
+#
+# async def test2(client):
+#     print("test1")
+#     homePageUrl = "https://www.zhihu.com/people/li-fang-quan-1/followers"
+#     resp = await client.get(homePageUrl)
+#     print("test2")
+#     print(await resp.text())
+#
+#
+# if __name__=='__main__':
+#     file = open(os.path.join(os.path.dirname(__file__), "cookie.txt"), 'rb')
+#     cookies = pickle.load(file)
+#     file.close()
+#     conn = aiohttp.ProxyConnector("http://222.176.112.10:80")
+#     client = aiohttp.ClientSession(connector=conn, headers=headers)
+#     client._cookies = cookies
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(asyncio.wait([test(client), test2(client)]))
 
 
 
-def init_session():
-    session = requests.Session()
-    url = "http://www.zhihu.com/login/email"
-    login_resp = requests.post(url, data=data, headers=headers)
-    session.cookies.update(login_resp.cookies)
-    if login_resp.json()["msg"] == '登陆成功':
-        logger.info("-------------登陆成功-----------------")
-    else:
-        logger.info(login_resp.json()["msg"])
-    return session
+# def init_session():
+#     session = requests.Session()
+#     url = "http://www.zhihu.com/login/email"
+#     login_resp = requests.post(url, data=data, headers=headers)
+#     session.cookies.update(login_resp.cookies)
+#     if login_resp.json()["msg"] == '登陆成功':
+#         logger.info("-------------登陆成功-----------------")
+#     else:
+#         logger.info(login_resp.json()["msg"])
+#     return session
 
 
 @log_time("handle follow.")
-def handle_follow(session, origin_user):
+async def handle_follow(origin_user):
+    print("begin to handle user:{0}".format(origin_user.user_id))
     logger.info("begin to handle user:{0}".format(origin_user.user_id))
     update_user_status(origin_user, 1)
     item_flag = 0
     while 1:
         item_flag += 1
         try:
-            handle_follow_item(session, origin_user, 'ee')
+            await handle_follow_item(origin_user, 'ee')
         except Exception as e:
-            sent_email("Crawler Exception. handle_followee_item :<br />", traceback.format_exc())
+            #sent_email("Crawler Exception. handle_followee_item :<br />", traceback.format_exc())
             logger.exception("retry {0}. time:".format("handle_followee_item", item_flag))
             if item_flag > 5:
                 raise e
@@ -121,9 +125,9 @@ def handle_follow(session, origin_user):
     while 1:
         item_flag += 1
         try:
-            handle_follow_item(session, origin_user, 'er')
+            await handle_follow_item(origin_user, 'er')
         except Exception as e:
-            sent_email("Crawler Exception. handle_follower_item :<br />", traceback.format_exc())
+            #sent_email("Crawler Exception. handle_follower_item :<br />", traceback.format_exc())
             logger.exception("retry {0}. time:".format("handle_follower_item", item_flag))
             if item_flag > 5:
                 raise e
@@ -135,11 +139,16 @@ def handle_follow(session, origin_user):
 
 
 @log_time("handle follow item.")
-def handle_follow_item(session, origin_user, follow_type):
-    global headers
-    user_list = [origin_user]
+async def handle_follow_item(origin_user, follow_type):
+    headers = {"Host": "www.zhihu.com",
+           "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36",
+           "Referer": "http://www.zhihu.com/",
+           "Origin": "http://www.zhihu.com",
+           "accept-encoding": "gzip, deflate"
+           }
     homepage = "http://www.zhihu.com/people/{0}/follow{1}s".format(origin_user.user_id, follow_type)
-    user_detail = get_user_detail(session, homepage)
+    user_list = [origin_user]
+    user_detail = await get_user_detail(homepage)
     logger.info("followee count:{0},follower count:{1} ".format(str(user_detail.followee_count),
                                                                 str(user_detail.follower_count)))
     if follow_type == 'ee':
@@ -155,11 +164,12 @@ def handle_follow_item(session, origin_user, follow_type):
         offset = 0
         while offset < follow_count:
             logger.info("begin to do offset:{0}".format(offset))
-            session.proxies = {}
+            #session.proxies = {}
             params['offset'] = offset
             follow_data['params'] = json.dumps(params)
-            resp = get_response(session, follow_url, follow_data, headers)
-            soup = BeautifulSoup(str(resp.json()['msg']), "html.parser", from_encoding='UTF-8',
+
+            resp = await get_response(follow_url, follow_data, headers)
+            soup = BeautifulSoup(str(resp['msg']), "html.parser", from_encoding='UTF-8',
                                  parse_only=SoupStrainer("a", class_='zg-link'))
             for item in soup.find_all('a'):
                 href = item['href']
@@ -177,9 +187,9 @@ def handle_follow_item(session, origin_user, follow_type):
 
 
 @log_time("request and parse homePage.")
-def get_user_detail(session, homepage):
-    resp = get_response(session, homepage, None, None, 'get')
-    homepage_soup = BeautifulSoup(resp.text, "html.parser", from_encoding='UTF-8')
+async def get_user_detail(homepage):
+    resp = await get_response(homepage, None, None, 'get')
+    homepage_soup = BeautifulSoup(resp, "html.parser", from_encoding='UTF-8')
     xsrf = homepage_soup.find("input", attrs={'name': '_xsrf'})['value']
     div_text = homepage_soup.find("div", class_='zh-general-list clearfix')
     if div_text is None:
@@ -193,25 +203,31 @@ def get_user_detail(session, homepage):
 
 
 @log_time("get response.")
-def get_response(session, follow_url, follow_data, headers, request_type='post'):
+async def get_response(follow_url, follow_data, headers, request_type='post'):
     retry_flag = 1
     while 1:
+        conn = aiohttp.TCPConnector(limit=100)
+        client = aiohttp.ClientSession(connector=conn)
+        client._cookies = cookies
         retry_flag += 1
         if retry_flag > 20:  #todo max retry time : 20 ip
             logger.error("retry max time. ")
             raise IOError("retry max time. ")
         try:
             if request_type == 'get':
-                resp = session.get(follow_url, data=follow_data, headers=headers, timeout=60)
+                async with client.get(follow_url, data=follow_data, headers=headers) as resp:
+                    content = await resp.text()
             else:
-                resp = session.post(follow_url, data=follow_data, headers=headers, timeout=60)
-            if resp.status_code != 200:
-                logger.error("error status:{0}".format(resp.status_code))
-                #time.sleep(5)
-                session.proxies = get_proxy()
+                async with client.post(follow_url, data=follow_data, headers=headers) as resp:
+                    content = await resp.json()
+            if resp.status != 200:
+                logger.error("error status:{0}".format(resp.status))
+                #session.proxies = get_proxy()
+                client._connector = get_aio_proxy()
             else:
-                return resp
+                return content
         except RequestException:
             logger.exception("get offset response exception.")
             #time.sleep(5)
-            session.proxies = get_proxy()
+            #session.proxies = get_proxy()
+            client._connector = get_aio_proxy()
